7e5dd5c47ce5ae90f7f6ea9e584393181bdfd1a8
Sergio Alves <salves2383@gmail.com>
2018-01-27 18:30:38 +0100

    Roman Numerals exercise

---
+++ roman-numerals/roman_numerals.rb
@@ -0,0 +1,18 @@
+class RomanNumerals
+
+ROMAN = {
+  'I' => 1,
+  'V' => 5,
+  'X' => 10,
+  'L' => 50,
+  'C' => 100,
+  'D' => 500,
+  'M' => 1000
+}.freeze
+
+  def self.to_roman(num)
+    result = ''
+
+  end
+
+end
