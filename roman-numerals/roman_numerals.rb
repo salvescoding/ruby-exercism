b272fafcdb03e9d9e70d1781f2338c2f86001ffd
Sergio Alves <salves2383@gmail.com>
2018-01-29 13:37:15 +0100

    Some changes on the code, still could not find solution

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,18 +1,27 @@
 class RomanNumerals
 
-ROMAN = {
-  'I' => 1,
-  'V' => 5,
-  'X' => 10,
-  'L' => 50,
-  'C' => 100,
-  'D' => 500,
-  'M' => 1000
-}.freeze
+  RN = {
+    1000 => "M",
+     900 => "CM",
+     500 => "D",
+     400 => "CD",
+     100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
+        0 => "",
+  }.freeze
 
-  def self.to_roman(num)
-    result = ''
-
+  def to_roman(num)
+    RN.map do |value, letter|
+      amt, num = num.divmod(value)
+      letter * amt
+    end.join
   end
 
 end
