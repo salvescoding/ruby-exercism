cd7f95ee09fbd49d61ab6fcefbd20b3b84a34e7d
Sergio Alves <salves2383@gmail.com>
2018-02-01 20:47:33 +0100

    Solution to convert fixnum to Roman numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,27 +1,19 @@
-class RomanNumerals
+class RomanNumerable
 
-  RN = {
-    1000 => "M",
-     900 => "CM",
-     500 => "D",
-     400 => "CD",
-     100 => "C",
-      90 => "XC",
-      50 => "L",
-      40 => "XL",
-      10 => "X",
-        9 => "IX",
-        5 => "V",
-        4 => "IV",
-        1 => "I",
-        0 => "",
-  }.freeze
+NEW_DIVIDERS = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+NEW_CHAR = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
 
-  def to_roman(num)
-    RN.map do |value, letter|
-      amt, num = num.divmod(value)
-      letter * amt
-    end.join
+  def to_roman
+    num = self
+    roman = ""
+    NEW_DIVIDERS.each_with_index do |divider, index|
+      t = num / divider
+      t.times do
+        roman.insert(-1, NEW_CHAR[index])
+      end
+      num = num % divider
+    end
+   roman
   end
 
 end
