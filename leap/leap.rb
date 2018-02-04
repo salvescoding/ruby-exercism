ec540d94c5f9cad3740bf0fc7c2c1ee217a77bff
Sergio Alves <salves2383@gmail.com>
2018-02-04 12:50:41 +0100

    Solution for Leap Year exercise

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,1 +1,17 @@
-leap.rb
+module BookKeeping
+  VERSION = 3
+end
+
+class Year
+
+  def self.leap?(year)
+    if year % 4 == 0
+      if (year % 100 == 0) && !(year % 400 == 0)
+        false
+      else
+        true
+      end
+    end
+  end
+
+end
