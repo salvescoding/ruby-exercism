b32883ce34f4b56b111e11a7b58d4fa01666af68
Sergio Alves <salves2383@gmail.com>
2018-03-08 18:53:44 +0100

    Added work cout kata, still not finish, a 3 tests still need to be passed

---
+++ word-count/word_count.rb
@@ -0,0 +1,11 @@
+class Phrase
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def word_count
+    words = @phrase.split(/\W+/)
+    h = words.each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1 }
+  end
+end
