5e175d9be8189ca70b24ce6df27bd406859dda00
Sergio Alves <salves2383@gmail.com>
2018-03-18 22:14:42 +0100

    Inserted some comments on the code to be all documented

--- MarsroverRubySA/lib/controller.rb
+++ MarsroverRubySA/lib/controller.rb
@@ -3,6 +3,7 @@ require_relative 'rover'
 
 class Controller
 
+  # Initialize the controller with 4 intance variables that will hold the values from the input file
   def initialize
     @input = []
     @rovers = []
@@ -10,35 +11,45 @@ class Controller
     @map = []
   end
 
+  # This method will parse the file and go through every line and store it on the variable input
   def read_file(input)
     File.open(input).each do |row|
       @input << row.chomp
     end
   end
 
+  # Argument error in case there is no coordinates for map
+  # calls a method that returns the correct coordinates for map, and instance a new map.
   def create_map
-    raise ArgumentError.new("Invalid map coordinates") if get_map_size == ''
+    raise ArgumentError.new("Invalid map coordinates") if get_map_size == []
     map = get_map_size.join("")
     @map = Map.new(map.split(" ")[0].to_i, map.split(" ")[1].to_i)
   end
 
+  # # Argument error in case there is no rovers initial coordinates for rovers
+  # calls a method that returns the correct set of initial coordinates for rovers
   def get_rovers_position
     raise ArgumentError.new("Invalid rovers coordinates") if get_rovers_initial_position == []
     rovers = get_rovers_initial_position
     landing_rovers(rovers)
   end
 
+  #  Argument error in case there is instructions for rovers
+  # calls a method that returns the correct set of instructions
   def get_instructions
     raise ArgumentError.new("Invalid instructions") if get_rovers_instructions == []
     @instructions = get_rovers_instructions
   end
 
+  # Go through each rover, and for each rover send the instructions that belong to the
+  # same index has the rover, rover[0] has instructions[0] and so on
   def move_rovers
     @rovers.each_with_index do |rover, index|
       rover.receives_instructions(@instructions[index])
     end
   end
 
+  # Outputs the final position of the Roverss
   def output
     @rovers.each do |rover|
       puts "Rover went out of the plateu!!!" unless rover.inside_plateu?(@map.x, @map.y)
